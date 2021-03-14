c8ed19acfed1f21aad9d42effc036a51167dec67
michaelw924 <michaelwhite924@gmail.com>
2021-03-13 21:20:07 -0600

    Updating plot progress more, need to add in proper average calculations based on sectioned area

--- 335Lab/Lab 2/MATLAB/graph3D.m
+++ 335Lab/Lab 2/MATLAB/graph3D.m
@@ -13,7 +13,7 @@ mph40 = figure;
 zeros = linspace(0,0,height(data.y40));
 scatter3(data.Position,zeros,data.x40);
 hold on;
-% plot3(data.Position*cos(angles),data.Position*sin(angles),data.x40);
+plot3(data.Position*cos(angles),data.Position*sin(angles),data.x40);
 % for i = 1:length(data.Position)
 %     plot3(data.Position(i)*cos(angles),data.Position(i)*sin(angles),linspace(data.x40(i),data.x40(i),length(angles)),'b');
 %     plot3(data.Position(i)*cos(angles),data.Position(i)*sin(angles),linspace(data.y40(i),data.y40(i),length(angles)),'b');
